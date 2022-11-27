cb6b8e405744da5cb3e145c71cbdbbefc73ee27b
Gustavo Esteven Fajardo Cruz <gfajardo1997@gmail.com>
2022-11-26 22:00:21 -0500

    finalizacion seccion 10 typeOrm

---
+++ README.md
@@ -0,0 +1,26 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
+</p>
+
+[circleci-image]: https://img.shields.io/circleci/build/github/nestjs/nest/master?token=abc123def456
+[circleci-url]: https://circleci.com/gh/nestjs/nest
+
+# Teslo API
+1.Clonar proyecto
+
+2.Instalar los modulos de node
+```
+yarn install
+````
+3.Clonar el archivo **.env.template** y renombralo a **.env**
+
+4.Renombrar las variables de entorno
+
+5.levantar la base de datos
+```
+docker-compose up -d
+```
+6.Levantar el modo desarrollo
+```
+yarn start:dev
+```
---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,39 @@
+import { Transform } from "class-transformer";
+import { IsArray, IsIn, IsInt, IsNumber, IsOptional, IsPositive, IsString, MinLength } from "class-validator";
+
+export class CreateProductDto {
+  @IsString()
+  @MinLength(1)
+  @Transform((value) => value.obj.title.toLowerCase())
+  title: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  price?: number;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsOptional()
+  @IsString()
+  slug?: string;
+
+  @IsInt()
+  @IsPositive()
+  @IsOptional()
+  stock?: number;
+
+  @IsString({ each: true })
+  @IsArray()
+  sizes: string[];
+
+  @IsIn(["men", "women", "kid", "unisex"])
+  gender: string;
+
+  @IsString({ each: true })
+  @IsArray()
+  @IsOptional()
+  tags:string[]
+}
